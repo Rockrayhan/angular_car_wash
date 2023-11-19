f4d2e79538e6c78b75e0182c87785f75a23b4e3f
rockRayhan <khayrulalamdict@gmail.com>
2023-11-19 15:01:23 +0600

    contact form

--- api/contact.php
+++ api/contact.php
@@ -3,13 +3,16 @@ include_once('database.php');
 $postdata = file_get_contents("php://input");
 $result =  json_decode($postdata);
 
-if (isset($result->name) && isset($result->email) && isset($result->message)) {
+if (isset($result->name) && isset($result->email) && isset($result->message) && isset($result->subject)  ) {
 
     $name =  $result->name;
     $email =  $result->email;
+    $subject =  $result->subject;
     $message =  $result->message;
 
-    $db->query("INSERT INTO contact VALUES(NULL, '$name' , '$email', '$message')");
+
+    $db->query("INSERT INTO tblenquiry VALUES(NULL, '$name' , '$email', '$subject', '$message', now(), '')");
+
 
     if ($db->affected_rows > 0) {
         echo json_encode(['msg' => "Successfully Inserted"]);
